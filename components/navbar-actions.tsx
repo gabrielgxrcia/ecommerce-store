99e8d547b1d0e9d4822d9cad7cf1cf6ce20af7a7
Gabriel <gabrielgrazeffi123@gmail.com>
2023-07-25 00:14:09 -0300

    add to cart funcionality

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 import Button from "@/components/ui/button"
+import useCart from "@/hooks/use-cart"
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -12,6 +13,8 @@ const NavbarActions = () => {
   useEffect(() => {
     setIsMounted(true)
   }, [])
+
+  const cart = useCart()
 
   const router = useRouter()
 
@@ -26,7 +29,9 @@ const NavbarActions = () => {
         className="flex items-center rounded-full bg-black px-4 py-2"
       >
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white"></span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   )
